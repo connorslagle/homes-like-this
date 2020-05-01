e69786da95635b41dbff83d89fa2ef0887caf737
connorslagle <connor.slagle@colorado.edu>
2020-05-01 17:39:15 -0600

    building webdriver

--- src/webscraper.py
+++ src/webscraper.py
@@ -1,5 +1,13 @@
 from selenium import webdriver
 from wayback import WaybackClient, WaybackSession
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
 
+
+
+if __name__ == "__main__":
+    live_url = 'https://www.realtor.com/realestateandhomes-search/Aurora_CO'
+
+    client = WaybackClient()
+    records = client.search(live_url)
 
