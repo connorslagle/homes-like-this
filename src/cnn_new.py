bf27a17c21422aa4c1d4da03470a056561f5c156
connorslagle <connor.slagle@colorado.edu>
2020-06-02 22:01:35 -0600

    ae class fin

--- src/cnn_new.py
+++ src/cnn_new.py
@@ -2,18 +2,27 @@
 import numpy as np
 import pickle
 from datetime import datetime
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+plt.rcParams.update({'font.size': 18})
 
 # tf imports
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.callbacks import TensorBoard
+
+# sk imports
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
+from sklearn.metrics.pairwise import cosine_distances
 
 
 class Autoencoder():
-    '''
-    This class will build CNN autoencoder.
-    '''
     def __init__(self):
+        '''
+        This class will build CNN autoencoder.
+        '''
         self._clear_variables()
 
     def _clear_variables(self):
@@ -26,15 +35,13 @@ class Autoencoder():
         self.encoder_decoder = None
         self.config = None
 
-
-    def use_gpu(self):
+    def _use_gpu(self):
         '''
         Sets tf environment for gpu operation
         '''
         self.config = tf.compat.v1.ConfigProto()
         self.config.gpu_options.allow_growth = True
         tf.compat.v1.Session(config=self.config)
-
 
     def build_autoencoder(self, use_color_img=True):
         '''
@@ -53,7 +60,7 @@ class Autoencoder():
         layer_list = []
         for encode_layer in range(num_encode_layers):
             if encode_layer == 0:
-                encode_1 = keras.layers.Conv2D(
+                encode_1 = layers.Conv2D(
                     filters=init_num_filters,
                     kernel_size=(3,3),
                     strides=(1,1),
@@ -65,7 +72,7 @@ class Autoencoder():
                 layer_list.append(encode_1)
                 
                 layer_list.append(
-                    keras.layers.MaxPooling2D(
+                    layers.MaxPooling2D(
                         pool_size=(2,2),
                         padding='same'
                     )(layer_list[-1])
@@ -73,7 +80,7 @@ class Autoencoder():
 
             else:
                 layer_list.append(
-                    keras.layers.Conv2D(
+                    layers.Conv2D(
                         filters=(init_num_filters // (2**encode_layer)),
                         kernel_size=(3,3),
                         strides=(1,1),
@@ -83,7 +90,7 @@ class Autoencoder():
                 )
 
                 layer_list.append(
-                    keras.layers.MaxPooling2D(
+                    layers.MaxPooling2D(
                         pool_size=(2,2),
                         padding='same'
                     )(layer_list[-1])
@@ -91,13 +98,13 @@ class Autoencoder():
         
 
         layer_list.append(
-            keras.layers.SpatialDropout2D(
+            layers.SpatialDropout2D(
                 rate=0.5
             )(layer_list[-1])
         )
 
         layer_list.append(
-            keras.layers.Flatten()(layer_list[-1])
+            layers.Flatten()(layer_list[-1])
         )
 
         self.encoder = keras.Model(inputs, layer_list[-1])
@@ -105,14 +112,14 @@ class Autoencoder():
         self.latent = self.encoder.output
 
         layer_list.append(
-            keras.layers.Reshape(
+            layers.Reshape(
                 target_shape=(4,4,8)
             )(layer_list[-1])
         )
 
         for decode_layer in range(num_encode_layers)[::-1]:
             layer_list.append(
-                keras.layers.Conv2D(
+                layers.Conv2D(
                     filters=(init_num_filters // (2**decode_layer)),
                     kernel_size=(3,3),
                     strides=(1,1),
@@ -122,13 +129,13 @@ class Autoencoder():
             )
 
             layer_list.append(
-                keras.layers.UpSampling2D(
+                layers.UpSampling2D(
                     size=(2,2)
                 )(layer_list[-1])
             )
 
         layer_list.append(
-            keras.layers.Conv2D(
+            layers.Conv2D(
                     filters=3,
                     kernel_size=(3,3),
                     strides=(1,1),
@@ -143,33 +150,59 @@ class Autoencoder():
             loss='mean_squared_error'
         )
 
-    
-    def fit_(self, X_train, X_test, num_epochs, batch_size_, callbacks=None):
+    def fit_(self, X_train, X_test, num_epochs, batch_size_, use_gpu=True, with_tensorboard=True):
         '''
         Fits Autoencoder to data
         '''
-        self.NAME = "new_ae_10eps_10batch_128_5down5up_50do_128feats_listings"
+        self.NAME = "new_ae_{}eps_{}batch_128_5down5up_50do_128feats_listings".format(
+            num_epochs, batch_size_
+        )
 
-        if callbacks is not None:
+        if use_gpu:
+            self._use_gpu()
 
+        if with_tensorboard:
             self.encoder_decoder.fit(X_train, X_train,
                 epochs=num_epochs,
                 batch_size=batch_size_,
                 validation_data=(X_test,X_test),
-                callbacks=callbacks)
+                callbacks=TensorBoard(log_dir='../logs/{}'.format(self.NAME)))
         else:
             self.encoder_decoder.fit(X_train, X_train,
                 epochs=num_epochs,
                 batch_size=batch_size_,
                 validation_data=(X_test,X_test))
             
-
-    def kmean_cluster(self, num_clusters):
+    def kmean_cluster(self, X_test, num_clusters):
         '''
         Cluster encoded images to means
         '''
-        pass
+        self.kmeans = KMeans(n_clusters=num_clusters)
+        self.kmeans.fit(X_test)
+    
+    def elbow_plot(self, X_test, max_k, f_name):
+        '''
+        Plot elbow plot
+        '''
+        fig, ax = plt.subplots(1, figsize=(12,12))
 
+        rss_lst = []
+        for k in range(1, max_k):
+            self.kmean_cluster(X_test, k)
+            rss_lst.append(self.kmeans.inertia_)
+        
+        ax.plot(range(1, max_k), rss_lst)
+        ax.set_ylabel('RSS')
+        ax.set_xlabel('Number of Clusters')
+        ax.set_title('Elbow Plot for Model:\n{}'.format(self.NAME))
+
+        self._save_fig('elbow_{}.png'.format(self.NAME))
+
+    def silhouette_plot(self):
+        '''
+        Plots silhouette plot, fill in later.
+        '''
+        pass
 
     def save_model(self):
         '''
@@ -184,14 +217,22 @@ class Autoencoder():
         ), 'wb') as f:
             pickle.dump(self.latent,f)
 
-
     def load_model(self,model_fname, latent_fname):
         '''
         Load previously saved model and latent features.
         '''
+        self._clear_variables()
+
         self.encoder_decoder = keras.models.load_model('../models/{}'.format(model_fname))
         with open('../models/{}'.format(latent_fname), 'rb') as f:
             self.latent = pickle.load(f)
+    
+    def _save_fig(self, file_name):
+        '''
+        Saves and closes figure.
+        '''
+        plt.savefig('../images/{}'.format(file_name), dpi=100)
+        plt.close('all')
 
 
 if __name__ == "__main__":
