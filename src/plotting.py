163a3604db77d06fbf5f21f59ceda96e6f2dc45e
connorslagle <connor.slagle@colorado.edu>
2020-06-04 21:51:30 -0600

    bed

---
+++ src/plotting.py
@@ -0,0 +1,18 @@
+from data_pipelines import ImagePipeline
+
+def metadata_heatmap():
+    pass
+
+def sat_histogram():
+    '''
+    Make 
+    '''
+    pass
+
+def tt_holdout_error_curves():
+    '''
+    Test, train and holdout error curves.
+    '''
+
+if __name__ == "__main__":
+    pass
