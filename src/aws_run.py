a79e09f7ccdb139c6348e96f0bd7b2b3e7d91564
connorslagle <connor.slagle@colorado.edu>
2021-01-27 11:29:57 -0700

    couple of xception runs

--- src/aws_run.py
+++ src/aws_run.py
@@ -3,11 +3,13 @@ import argparse
 import pickle
 import numpy as np
 
+from sklearn.model_selection import train_test_split
+
 # other file imports
 from cnn_new import Autoencoder
 from cnn_w_meta import XceptionAE
 
-from data_pipelines import ImagePipeline
+# from data_pipelines import ImagePipeline
 
 
 if __name__ == "__main__":
@@ -26,8 +28,8 @@ if __name__ == "__main__":
     epochs = int(args.epochs)
     batch_1 = int(args.batchsize)       # around 30-40
     kernel_sizes = [(3,3),(4,4),(5,5)]
-    layers = 3
-    init_filter = 128
+    layers = 5
+    init_filter = 64
 
     epoch_list = np.array([epochs//2, epochs, epochs*2]).astype(int)
     batch_list = np.array([batch_1-10, batch_1, batch_1+10]).astype(int)
@@ -35,30 +37,40 @@ if __name__ == "__main__":
     
     # for _ in range(3):
     if use_gray:
-        # load data
-        pipeline = ImagePipeline('../data/proc_imgs/128/gray')
-        pipeline.build_Xy(set_seed=False)
-        X_train, X_test = pipeline.X_train, pipeline.X_test
-
-        # build model
-        model = Autoencoder()
-
         # Select model
         if args.model == 'XceptionAE':
             model_builder = XceptionAE()
         elif args.model == 'Autoencoder':
             model_builder = Autoencoder()
-    else:
-        # load data
-        pipeline = ImagePipeline('../data/proc_imgs/128/color', gray_imgs=False)
-        pipeline.build_Xy(set_seed=False)
-        X_train, X_test = pipeline.X_train, pipeline.X_test
 
+        # load data from file
+        model_builder.load_Xy('2020-06-10')
+
+        X = model_builder.X_total
+
+        X_tt, X_holdout = train_test_split(X, random_state=33)
+        X_train, X_test = train_test_split(X_tt, random_state=33)
+
+        X_train = X_train.reshape(X_train.shape[0], 128, 128, 1)
+        X_test = X_test.reshape(X_test.shape[0], 128, 128, 1)
+
+    else:
         # Select model
         if args.model == 'XceptionAE':
             model_builder = XceptionAE(gray_imgs=False)
         elif args.model == 'Autoencoder':
             model_builder = Autoencoder(gray_imgs=False)
+
+        # load data from file
+        model_builder.load_Xy('2020-06-10')
+
+        X = model_builder.X_total
+
+        X_tt, X_holdout = train_test_split(X, random_state=33)
+        X_train, X_test = train_test_split(X_tt, random_state=33)
+
+        X_train = X_train.reshape(X_train.shape[0], 128, 128, 3)
+        X_test = X_test.reshape(X_test.shape[0], 128, 128, 3)
 
     model_builder.build_autoencoder(init_filter, layers, enc_do=0, dec_do=0)
 
@@ -66,4 +78,4 @@ if __name__ == "__main__":
     model_builder.fit_(X_train, X_test, epochs, batch_1)
     
     # save
-    model_builder.save_model()
+    model_builder.save_model(args.model)
